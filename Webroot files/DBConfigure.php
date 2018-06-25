7ec92a54b128057155131cdf5f99888e25635364
Beardsmith <38120513+Beardsmith@users.noreply.github.com>
2018-06-25 18:20:11 +1000

    removed password from file
    removed personal password from file

--- Webroot files/DBConfigure.php
+++ Webroot files/DBConfigure.php
@@ -1,6 +1,6 @@
 <?PHP
 
-$link = mysql_connect('localhost','root','redoctober9');
+$link = mysql_connect('localhost','root','[your password here');
 if (!$link) {
     die('Could not connect: ' . mysql_error());
 }
